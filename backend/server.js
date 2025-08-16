1bcaf1efbb394cce70a7ff457d7374e28df6fa46
PrathamSingh1 <prathamjii36@gmail.com>
2025-08-16 18:56:16 +0000

    Added user register and login

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from "./Routes/user.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// home testing route
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'This is home route'
+    })
+})
+
+
+// user Router
+app.use('/api/user', userRouter)
+
+mongoose.connect(process.env.DATABASE_URL,{
+    dbName:"Ecommerce"
+}).then(()=>{
+    console.log("Mongodb connected sucessfully");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+
+const port = 3000;
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
