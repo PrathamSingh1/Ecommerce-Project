279c59bff9132df52cfb1c8f8590b87763ded6b2
PrathamSingh1 <prathamjii36@gmail.com>
2025-08-25 16:09:16 +0000

    added filteredData

--- frontend/src/context/AppState.jsx
+++ frontend/src/context/AppState.jsx
@@ -5,6 +5,9 @@ import { Bounce, ToastContainer, toast } from 'react-toastify';
 
 const AppState = (props) => {
   const [products, setProducts] = useState([]);
+  const [token, setToken] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [filteredData, setFilteredData] = useState([]);
   // const data = 10;
   useEffect(() => {
     const fetchProduct = async () => {
@@ -16,9 +19,10 @@ const AppState = (props) => {
       })
       console.log(api.data.products);
       setProducts(api.data.products)
+      setFilteredData(api.data.products)
     }
     fetchProduct();
-  }, []);
+  }, [token]);
 
   // register user
   const register = async (name, email, password) => {
@@ -44,10 +48,44 @@ const AppState = (props) => {
     return api.data
   }
 
+  // login user
+  const login = async (email, password) => {
+    const api = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/login`,{ email, password}, {
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      withCredentials: true
+    })
+
+    toast.success(api.data.message, {
+    position: "top-right",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+    });
+
+    setToken(api.data.token)
+    setIsAuthenticated(true)
+    localStorage.setItem('token', token)
+    return api.data
+  }
+
+
   return (
     <AppContext.Provider value={{
         products,
-        register
+        register,
+        login,
+        token,
+        setIsAuthenticated,
+        isAuthenticated,
+        filteredData,
+        setFilteredData
     }}>{props.children}</AppContext.Provider>
   )
 }
